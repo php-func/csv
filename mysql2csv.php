cf7f534906b896e336643d3ebf83c9686a135348
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2022-10-03 05:52:40 +0200

    Create mysql2csv.php

---
+++ mysql2csv.php
@@ -0,0 +1,22 @@
+<?php
+$conn = mysqli_connect("localhost","root","test", "phppot_examples");
+
+$query = "SELECT * FROM toy";
+$result = mysqli_query($conn, $query);
+
+$num_column = mysqli_num_fields($result);		
+
+$csv_header = '';
+for($i=0;$i<$num_column;$i++) {
+    $csv_header .= '"' . mysqli_fetch_field_direct($result,$i)->name . '",';
+}	
+$csv_header .= "\n";
+
+$csv_row ='';
+while($row = mysqli_fetch_row($result)) {
+for($i=0;$i<$num_column;$i++) {
+		$csv_row .= '"' . $row[$i] . '",';
+}
+	$csv_row .= "\n";
+}
+?>
