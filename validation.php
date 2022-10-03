fa4f5213b796176c66c29f2439636116e508fc3e
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2022-10-03 05:50:09 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -1,3 +1,58 @@
+<?php
+if (isset($_POST["upload"])) {
+    
+    // Get file extension
+    $file_extension = pathinfo($_FILES["file-input"]["name"], PATHINFO_EXTENSION);
+    // Validate file input to check if is not empty
+    if (! file_exists($_FILES["file-input"]["tmp_name"])) {
+        $response = array(
+            "type" => "error",
+            "message" => "File input should not be empty."
+        );
+    } // Validate file input to check if is with valid extension
+    else if ($file_extension != "csv") {
+            $response = array(
+                "type" => "error",
+                "message" => "Invalid CSV: File must have .csv extension."
+            );
+            echo $result;
+        } // Validate file size
+    else if (($_FILES["file-input"]["size"] > 2000000)) {
+            $response = array(
+                "type" => "error",
+                "message" => "Invalid CSV: File size is too large."
+            );
+        } // Validate if all the records have same number of fields
+    else {
+        $lengthArray = array();
+        
+        $row = 1;
+        if (($fp = fopen($_FILES["file-input"]["tmp_name"], "r")) !== FALSE) {
+            while (($data = fgetcsv($fp, 1000, ",")) !== FALSE) {
+                $lengthArray[] = count($data);
+                $row ++;
+            }
+            fclose($fp);
+        }
+            
+        $lengthArray = array_unique($lengthArray);
+        
+        if (count($lengthArray) == 1) {
+            $response = array(
+                "type" => "success",
+                "message" => "File Validation Success."
+            );
+        } else {
+            $response = array(
+                "type" => "error",
+                "message" => "Invalid CSV: Count mismatch."
+            );
+        }
+    }
+}
+?>
+
+
 <form id="frm-upload" action="" method="post"
     enctype="multipart/form-data">
     <div class="form-row">
